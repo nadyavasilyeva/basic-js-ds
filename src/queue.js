05bcd6dee70a99e4f7e0ce4ac959d71720da6a1c
Надежда Васильева <nadyanadyaspe@gmail.com>
2023-04-17 22:10:01 +0300

    test

--- src/queue.js
+++ src/queue.js
@@ -13,21 +13,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
+
 class Queue {
 
+  constructor() {
+    this.first = null
+    this.last = null
+    this.length = 0   
+  }
+  
+  isEmpty() {
+    return this.first == null;
+  }
+
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.first
   }
+  enqueue(value) {
+    const newNode = new ListNode(value);
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.isEmpty()) {
+      this.first = newNode;
+      this.last = newNode;
+    }
+    else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this ;
   }
-
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    if (this.isEmpty()) return null;
+
+    const itemToDel = this.first;
+
+    if (this.first === this.last) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.length--;
+    return itemToDel;
   }
 }
 
