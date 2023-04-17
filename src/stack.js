9fed9da3456c323f7738bd2c937516b18d689947
Надежда Васильева <nadyanadyaspe@gmail.com>
2023-04-17 22:21:31 +0300

    test add

--- src/stack.js
+++ src/stack.js
@@ -12,20 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
-class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Stack{
+  constructor(... args ){
+    this.store = [... args.reverse()];
   }
-
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  peek(){
+    return this.store[0];
   }
-
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  push(value){
+    return this.store.unshift(value);
+  }
+  pop(){
+   return this.store.shift();
   }
 }
 
